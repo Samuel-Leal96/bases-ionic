eeac67248c1392685559758cf241e0b745826f17
Samuel-Leal96 <100164814+SamLeal96@users.noreply.github.com>
2023-05-22 23:16:28 -0600

    Section 4 complete Bases Ionic

---
+++ src/app/pages/action-sheet/action-sheet.page.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-action-sheet',
+  templateUrl: './action-sheet.page.html',
+  styleUrls: ['./action-sheet.page.scss'],
+})
+export class ActionSheetPage {
+
+}
